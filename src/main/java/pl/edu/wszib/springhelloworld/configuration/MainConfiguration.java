ac60c45528c0f132a737d668abed75715bc3a91f
arzieba1420 <arzieba@wszib.edu.pl>
2019-02-09 12:42:37 +0100

    dsfsd

--- src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
+++ src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
@@ -1,5 +1,6 @@
 package pl.edu.wszib.springhelloworld.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.edu.wszib.springhelloworld.model.*;
@@ -7,22 +8,35 @@ import pl.edu.wszib.springhelloworld.model.*;
 @Configuration
 public class MainConfiguration {
 
+    @Bean("Klocki")
+    public Zabawka zabawka1(){
+        return new Klocki();
+    }
+    @Bean("Mis")
+    public Zabawka zabawka2(){
+        return new Mis();
+    }
+    @Bean("Samochodzik")
+    public Zabawka zabawka3(){
+        return new Samochodzik();
+    }
+
     @Bean
-    public Dziecko jas(Zabawka zabawka) {
+    public Dziecko jas(@Qualifier("Klocki") Zabawka zabawka) {
         Dziecko jas = new Jas(zabawka);
         jas.odpakuj();
         return jas;
     }
 
     @Bean
-    public Dziecko malgosia(Zabawka zabawka) {
+    public Dziecko malgosia(@Qualifier("Mis") Zabawka zabawka) {
         Dziecko malgosia = new Malgosia(zabawka);
         malgosia.odpakuj();
         return malgosia;
     }
 
     @Bean
-    public Dziecko grzes(Zabawka zabawka) {
+    public Dziecko grzes(@Qualifier("Samochodzik") Zabawka zabawka) {
         Dziecko grzes = new Grzes(zabawka);
         grzes.odpakuj();
         return grzes;
---
+++ src/main/java/pl/edu/wszib/springhelloworld/configuration/MyConfig.java
@@ -0,0 +1,4 @@
+package pl.edu.wszib.springhelloworld.configuration;
+
+public class MyConfig {
+}
